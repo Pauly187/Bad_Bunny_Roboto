878bbbde5e722d06500cf3e75ac2a8b1fea36595
Pauly <twitchpauly@gmail.com>
2021-05-01 19:21:07 +0200

    Max Beginning

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+#import ev3dev.ev3 as ev3
+#Max Änderungen
+#Hola Max, hier kannst du dein Code schreiben und auch Pushen, dann kann ich auch mal drüber schauen :)
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#m= ev3.LargeMotor(`outA´)
