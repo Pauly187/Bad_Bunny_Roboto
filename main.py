9d6a4ddb18bc0c4608467b5d11e73a80cd795b71
Pauly <twitchpauly@gmail.com>
2021-05-03 14:28:21 +0200

    TEst YT

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
-from time import sleep
+import time
 # let one wheel of the Marsrover move foreward
 
 # Connect the output A to the motor
 motor_left = ev3.LargeMotor('outA')
 motor_right = ev3.LargeMotor('outD')
-# move motor for 1000ms with 500dec/sec
-motor_left.run_timed(time_sp=3000, speed_sp=500)
-motor_right.run_timed(time_sp=3000, speed_sp=500)
-# wait till the motor has stopped
-motor_left.wait_while('running')
-motor_right.wait_while('running')
 
-ev3.Sound.speak("Me amo Bad Bunny")
-sleep(1)
+motor_left.run_forever(speed_sp=500)
+motor_right.run_forever(speed_sp=500)
+
+time.sleep(5.0)
+
+motor_left.stop()
+motor_right.stop()
+
+
