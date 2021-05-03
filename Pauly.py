fea9831f48e2a808cd4185d02c5139e578a86d42
Pauly <twitchpauly@gmail.com>
2021-05-03 20:02:26 +0200

    Task1_1Try

--- Pauly.py
+++ Pauly.py
@@ -1,18 +1,60 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
 import time
-# let one wheel of the Marsrover move foreward
 
-# Connect the output A to the motor
 m_l = ev3.LargeMotor('outA')
 m_r = ev3.LargeMotor('outD')
 
-m_l.run_timed(speed_sp=500)
-m_r.run_timed(speed_sp=500)
-
-time.sleep(5.0)
-
-motor_left.stop()
-motor_right.stop()
-
-
+m_l.run_timed(time_sp=2000, speed_sp=500)
+m_r.run_timed(time_sp=2000, speed_sp=500)
+m_l.stop()
+m_r.stop()
+#25cm nach Vorne gefahren
+m_r.run_timed(time_sp=1000, speed_sp=500)
+m_r.stop()
+#90° turn left
+m_l.run_timed(time_sp=2000, speed_sp=500)
+m_r.run_timed(time_sp=2000, speed_sp=500)
+m_l.stop()
+m_r.stop()
+#25cm nach links gefahren (Ecke links unten)
+m_l.run_timed(time_sp=1000, speed_sp=500)
+m_l.stop()
+#90° trun right (untenlinks)
+m_l.run_timed(time_sp=4000, speed_sp=500)
+m_r.run_timed(time_sp=4000, speed_sp=500)
+m_l.stop()
+m_r.stop()
+#50cm gefahren nach oben (Ecke oben links)
+m_l.run_timed(time_sp=1000, speed_sp=500)
+m_l.stop()
+#90° trun right (obenlinks)
+m_l.run_timed(time_sp=4000, speed_sp=500)
+m_r.run_timed(time_sp=4000, speed_sp=500)
+m_l.stop()
+m_r.stop()
+#50cm gefahren nach rechts (Ecke oben rechts)
+m_l.run_timed(time_sp=1000, speed_sp=500)
+m_l.stop()
+#90° trun right (obenrechts)
+m_l.run_timed(time_sp=4000, speed_sp=500)
+m_r.run_timed(time_sp=4000, speed_sp=500)
+m_l.stop()
+m_r.stop()
+#50cm gefahren nach unten (Ecke unten rechts)
+m_l.run_timed(time_sp=1000, speed_sp=500)
+m_l.stop()
+#90° turn right (untenrechts)
+m_l.run_timed(time_sp=2000, speed_sp=500)
+m_r.run_timed(time_sp=2000, speed_sp=500)
+m_l.stop()
+m_r.stop()
+#25cm gefahren bis erster Turn
+m_r.run_timed(time_sp=1000, speed_sp=500)
+m_r.stop()
+#90° turn left (facing home)
+m_l.run_timed(time_sp=2000, speed_sp=500)
+m_r.run_timed(time_sp=2000, speed_sp=500)
+m_l.stop()
+m_r.stop()
+#25cm gefahren bis Home
