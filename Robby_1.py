daa6d7889c11db881a150276316f1f85bbf24f9a
Pauly <twitchpauly@gmail.com>
2021-05-04 19:52:45 +0200

    Test_run_target

---
+++ Robby_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import ev3dev.ev3 as ev3
+import time
+
+m_l = ev3.LargeMotor('outA')
+m_r = ev3.LargeMotor('outD')
+
+m_l.run_target(500,720)
+m_r.run_target(500,720)
+m_l.wait_while('running')
+m_r.wait_while('running')
+m_l.stop()
+m_r.stop()
