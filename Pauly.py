150bdb9131ef77073b3c024ac411d544de585cc2
Pauly <twitchpauly@gmail.com>
2021-05-04 14:03:54 +0200

    NewTry

--- Pauly.py
+++ Pauly.py
@@ -7,51 +7,45 @@ m_r = ev3.LargeMotor('outD')
 
 m_l.run_timed(time_sp=2000, speed_sp=500)
 m_r.run_timed(time_sp=2000, speed_sp=500)
-m_l.stop()
-m_r.stop()
+
 #25cm nach Vorne gefahren
 m_r.run_timed(time_sp=1000, speed_sp=500)
-m_r.stop()
+
 #90° turn left
 m_l.run_timed(time_sp=2000, speed_sp=500)
 m_r.run_timed(time_sp=2000, speed_sp=500)
-m_l.stop()
-m_r.stop()
+
 #25cm nach links gefahren (Ecke links unten)
 m_l.run_timed(time_sp=1000, speed_sp=500)
-m_l.stop()
+
 #90° trun right (untenlinks)
 m_l.run_timed(time_sp=4000, speed_sp=500)
 m_r.run_timed(time_sp=4000, speed_sp=500)
-m_l.stop()
-m_r.stop()
+
 #50cm gefahren nach oben (Ecke oben links)
 m_l.run_timed(time_sp=1000, speed_sp=500)
-m_l.stop()
+
 #90° trun right (obenlinks)
 m_l.run_timed(time_sp=4000, speed_sp=500)
 m_r.run_timed(time_sp=4000, speed_sp=500)
-m_l.stop()
-m_r.stop()
+
 #50cm gefahren nach rechts (Ecke oben rechts)
 m_l.run_timed(time_sp=1000, speed_sp=500)
-m_l.stop()
+
 #90° trun right (obenrechts)
 m_l.run_timed(time_sp=4000, speed_sp=500)
 m_r.run_timed(time_sp=4000, speed_sp=500)
-m_l.stop()
-m_r.stop()
+
 #50cm gefahren nach unten (Ecke unten rechts)
 m_l.run_timed(time_sp=1000, speed_sp=500)
-m_l.stop()
+
 #90° turn right (untenrechts)
 m_l.run_timed(time_sp=2000, speed_sp=500)
 m_r.run_timed(time_sp=2000, speed_sp=500)
-m_l.stop()
-m_r.stop()
+
 #25cm gefahren bis erster Turn
 m_r.run_timed(time_sp=1000, speed_sp=500)
-m_r.stop()
+
 #90° turn left (facing home)
 m_l.run_timed(time_sp=2000, speed_sp=500)
 m_r.run_timed(time_sp=2000, speed_sp=500)
