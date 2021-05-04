ebb4114b24d45d0098dc17b998844d636486cfe9
Pauly <twitchpauly@gmail.com>
2021-05-04 21:04:43 +0200

    Same

--- Robby_1.py
+++ Robby_1.py
@@ -7,6 +7,10 @@ m_r = ev3.LargeMotor('outD')
 
 m_l.run_to_rel_pos(position_sp=360, speed_sp=500, stop_action="brake")
 m_r.run_to_rel_pos(position_sp=360, speed_sp=500, stop_action="brake")
+m_l.wait_while('running')
+m_r.wait_while('running')
+m_l.run_to_rel_pos(position_sp=360, speed_sp=500, stop_action="brake")
+m_l.wait_while('running')
 
 m_l.stop()
 m_r.stop()
