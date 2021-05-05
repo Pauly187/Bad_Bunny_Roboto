4fc5b619d762c18e71a3c780c7b3e18cd72f6cf0
Max Maeule <max-sebastian.maeule@student.reutlingen-university.de>
2021-05-05 13:53:04 +0200

    ultrasonic

--- main.py
+++
@@ -1,6 +0,0 @@
-#import ev3dev.ev3 as ev3
-#Max Änderungen
-#Hola Max, hier kannst du dein Code schreiben und auch Pushen, dann kann ich auch mal drüber schauen :)
-
-#m= ev3.LargeMotor(`outA´)
-#Super Training!
