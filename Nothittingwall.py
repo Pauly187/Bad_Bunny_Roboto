376a1d893101c7a055d99ea2faf301ebbf2f1256
Max Maeule <max-sebastian.maeule@student.reutlingen-university.de>
2021-05-05 16:31:31 +0200

    LANLAN

--- Nothittingwall.py
+++ Nothittingwall.py
@@ -15,15 +15,14 @@ us.mode='US-DIST-CM'
 #unit of distance cm
 units = us.units
 
-distance = us.value()
-
-while true:
+while True:
     distance = us.value
     m_r.run_forever(speed_sp=500)
     m_l.run_forever(speed_sp=500)
     if distance <= 5:
-    m_r.run_forever(speed_sp=0)
-    m_l.run_forever(speed_sp=0)
+        m_r.run_forever(speed_sp=0)
+        m_l.run_forever(speed_sp=0)
+
 
 
 
