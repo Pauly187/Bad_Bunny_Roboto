b555c868277128c483b16fe759539fdb6b820e29
Pauly <twitchpauly@gmail.com>
2021-05-05 17:05:49 +0200

    MrkFunkt

---
+++ UltraSonic.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import ev3dev.ev3 as ev3
+import time
+
+#motors
+m_l = ev3.LargeMotor('outA')
+m_r = ev3.LargeMotor('outD')
+
+#sensors
+us = ev3.UltrasonicSensor()
+
+#UltrasonicSensor in distance mode
+us.mode='US-DIST-CM'
+
+#unit of distance cm
+units = us.units
+
+while True:
+    distance = us.value
+    m_r.run_forever(speed_sp=500)
+    m_l.run_forever(speed_sp=500)
+    if distance <= 100:
+        m_r.run_forever(speed_sp=0)
+        m_l.run_forever(speed_sp=0)
+        break
+
