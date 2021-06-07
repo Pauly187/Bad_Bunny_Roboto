a1f3de64f2c4e812c931213bc108e18119b58d39
Pauly <twitchpauly@gmail.com>
2021-06-07 16:08:34 +0200

    CHallenge

--- Challenge.py
+++ Challenge.py
@@ -1,21 +1,45 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
 import time
+
+#motors
 m_l = ev3.LargeMotor('outA')
 m_r = ev3.LargeMotor('outD')
 
-cl = ev3.ColorSensor()
+#sensors
+us = ev3.UltrasonicSensor()
 
-cl.mode = 'COL-COLOR'
-colors = ('unknown','black','blue','green','yellow','red','white','brown')
-count = colors
+#UltrasonicSensor in distance mode
+us.mode='US-DIST-CM'
+
+#unit of distance cm
+units = us.units
+
 while True:
-    count = colors[cl.value()]
-    m_l.run_to_rel_pos(position_sp=2000, speed_sp=500, stop_action="brake")
-    m_r.run_to_rel_pos(position_sp=2000, speed_sp=500, stop_action="brake")
-
-    if count = 4:
-        m_l.stop()
-        m_r.stop()
+    distance = us.value
+    m_r.run_forever(speed_sp=300)
+    m_l.run_forever(speed_sp=300)
+    if distance > 100:
+        m_r.run_forever(speed_sp=0)
+        m_l.run_forever(speed_sp=0)
         break
-ev3.Sound.speak(colors[cl.value()])
+
+while True:
+    distance = us.value
+    m_r.run_forever(speed_sp=300)
+    m_l.run_forever(speed_sp=300)
+    if distance < 100:
+        m_r.run_forever(speed_sp=0)
+        m_l.run_forever(speed_sp=0)
+        break
+
+m_l.run_to_rel_pos(position_sp=-278, speed_sp=300, stop_action="brake")
+m_l.wait_while('running')
+
+m_l.run_to_rel_pos(position_sp=-520, speed_sp=300, stop_action="brake")
+m_r.run_to_rel_pos(position_sp=-520, speed_sp=300, stop_action="brake")
+m_l.wait_while('running')
+m_r.wait_while('running')
+
+m_r.run_to_rel_pos(position_sp=-100, speed_sp=300, stop_action="brake")
+m_r.wait_while('running')
